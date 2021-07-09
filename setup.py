8a4a2affa1081d0d2e19152ff569bbda6a1e3057
jamesbarciz <Fauxpasla93>
2021-07-08 21:30:24 -0500

    leave master branch

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dspt12-lambda-james',
+    version='0.0.1',
+    author='James Barciz',
+    author_email='james.barciz@gmail.com',
+    keywords=['twitter', 'python', 'nlp'],
+    packages=['twitter'],
+    python_requires='>=3.6, <4',
+    install_requires=['']
+)
